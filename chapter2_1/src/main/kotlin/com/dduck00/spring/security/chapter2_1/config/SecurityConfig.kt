e0c5702780510ce73684036af69d76294c8e3626
dduck00 <ejr075@gmail.com>
2023-01-04 23:14:36 +0900

    impl SecurityFilterChain

--- chapter2_1/src/main/kotlin/com/dduck00/spring/security/chapter2_1/config/SecurityConfig.kt
+++ chapter2_1/src/main/kotlin/com/dduck00/spring/security/chapter2_1/config/SecurityConfig.kt
@@ -2,13 +2,17 @@ package com.dduck00.spring.security.chapter2_1.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig {
     @Bean
     fun userDetailsService(): UserDetailsService {
@@ -27,5 +31,13 @@ class SecurityConfig {
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return NoOpPasswordEncoder.getInstance()
+    }
+
+    @Bean
+    fun filterChainBasic(http: HttpSecurity): SecurityFilterChain {
+        http.httpBasic()
+        http.authorizeHttpRequests().anyRequest().permitAll()
+
+        return http.build()
     }
 }
